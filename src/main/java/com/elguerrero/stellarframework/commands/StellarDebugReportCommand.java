326dab3f4aa4177e3fe78cb7cb09968b7107a059
Elguerrero|MoonWalker <37558093+Elguerrero11@users.noreply.github.com>
2023-02-19 23:40:15 +0100

    Small change for test debugreport command

--- src/main/java/com/elguerrero/stellarframework/commands/StellarDebugReportCommand.java
+++ src/main/java/com/elguerrero/stellarframework/commands/StellarDebugReportCommand.java
@@ -1,6 +1,5 @@
 package com.elguerrero.stellarframework.commands;
 
-import com.elguerrero.stellarframework.StellarPluginFramework;
 import com.elguerrero.stellarframework.utils.DebugReport;
 import com.elguerrero.stellarframework.utils.StellarUtils;
 import dev.jorel.commandapi.CommandAPICommand;
@@ -9,7 +8,7 @@ import dev.jorel.commandapi.CommandAPICommand;
 public abstract class StellarDebugReportCommand {
 
 	public static void registerDebugReportCommand() {
-		new CommandAPICommand(StellarPluginFramework.getPLUGIN_NAME() + "--debugreport")
+		new CommandAPICommand("--debugreport")
 				.withRequirement(StellarUtils::senderIsConsole)
 				.executesConsole((sender, args) -> {
 
