6ae66c070415a69b91901f4772db6174c7c1bed7
Elguerrero|MoonWalker <37558093+Elguerrero11@users.noreply.github.com>
2023-04-01 21:20:37 +0200

    Small test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    kotlin("jvm") version "1.6.10"
-    `kotlin-dsl`
     `maven-publish`
 }
 
 group = "com.elguerrero.stellarframework"
-version = "1.6.19"
+version = "1.6.20"
 description = "A framework for spigot/paper plugins."
 
 repositories {
