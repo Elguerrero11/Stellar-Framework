8358959df066f12e98eca79e1fe435861cc8d35b
Elguerrero|MoonWalker <37558093+Elguerrero11@users.noreply.github.com>
2023-04-05 23:20:02 +0200

    Small test

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,13 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.26")
+
     annotationProcessor("org.projectlombok:lombok:1.18.26")
+
+    compileOnly("org.projectlombok:lombok:1.18.26")
     compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+
     implementation("dev.dejvokep:boosted-yaml:1.3.1")
     implementation("dev.jorel:commandapi-shade:8.7.5")
     implementation("commons-io:commons-io:2.11.0")
@@ -30,15 +33,14 @@ dependencies {
 
 tasks {
     shadowJar.configure {
-
         listOf(
                 "dev.dejvokep.boosted-yaml",
                 "dev.jorel.commandapi-shade",
-                "org.apache.commons-io"
         ).forEach { packageName ->
             val name = packageName.substringAfterLast('.')
             relocate(packageName, "libs.$name")
         }
+        relocate("org.apache.commons.io", "libs.commons-io")
     }
 }
 
@@ -53,13 +55,14 @@ publishing {
     }
 }
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-}
-
-
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+
