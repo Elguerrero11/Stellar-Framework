8bd415800316eefb485fcbb5db61e44ee926d533
Elguerrero|MoonWalker <37558093+Elguerrero11@users.noreply.github.com>
2023-04-05 19:33:12 +0200

    Test fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.elguerrero.stellarframework"
-version = "1.7.7"
+version = "1.7.8"
 description = "A framework for spigot/paper plugins."
 
 repositories {
@@ -41,7 +41,6 @@ tasks {
         }
     }
 }
-
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
